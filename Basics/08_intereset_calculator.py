878ab79feb438dbc5e4b1b78ca19d795b74aac40
Swarnava Neogi <swarnavaneogi@gmail.com>
2026-08-10 10:43:07 +0530

    basic loop fundamentals

--- Basics/08_intereset_calculator.py
+++ Basics/08_intereset_calculator.py
@@ -16,15 +16,15 @@ while True :
     ci=p*(1+(r/100))**t - p # compound interest formula
     total = si+p # total amount after adding simple interest to the principal
     cl_total = ci+p # total amount after adding compound interest to the principal
-    print(f"\nThe principal amount is : ${og}")
+    print(f"\nThe principal amount is :  ₹ {og}")
     print(f"The rate of interest is : {r}%")
     print(f"The time period is : {t} years")
     print("\n")
-    print(f"\nThe simple interest is : ${si}")
-    print(f"The compound interest is : ${ci}")
+    print(f"\nThe simple interest is :  ₹ {si}")
+    print(f"The compound interest is :  ₹ {ci}")
     print("\n")
-    print(f"The total amount after adding simple interest is : ${total}")
-    print(f"The total amount after adding compound interest is : ${cl_total}")
+    print(f"The total amount after adding simple interest is :  ₹ {total}")
+    print(f"The total amount after adding compound interest is :  ₹ {cl_total}")
 
     cont = input("Do you want to enter for another amount? (yes/no): ")
     if cont.lower() != 'yes':
