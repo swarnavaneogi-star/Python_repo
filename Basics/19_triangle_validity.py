055eb83fd3f3a2790c1c0b1c2f9ca63e740214f5
Swarnava Neogi <swarnavaneogi@gmail.com>
2026-08-16 10:26:48 +0530

    triangle validity checker

---
+++ Basics/19_triangle_validity.py
@@ -0,0 +1,14 @@
+while True:
+    a1=int(input("Enter the first angle of the triangle :"))
+    a2=int(input("Enter the second angle of the triangle :"))
+    a3=int(input("Enter the third angle of the triangle :"))
+
+    if a1 + a2 + a3 == 180:
+        print("The triangle is valid.")
+    else:
+        print("The triangle is not valid.")
+
+    cont = input("Do you want to enter another set of angles? (yes/no): ")
+    if cont.lower() != "yes":
+        print("Exiting the program ..")
+        break
