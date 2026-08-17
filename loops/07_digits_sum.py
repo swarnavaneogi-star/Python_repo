d1c8a4d9c0a41e86cbd9d0047a91313c54ec7c02
Swarnava Neogi <swarnavaneogi@gmail.com>
2026-08-17 15:33:30 +0530

    digits sum and counting

--- loops/07_digits_sum.py
+++ loops/07_digits_sum.py
@@ -1,12 +1,15 @@
 while True:
     num=int(input("Enter a  number :"))
     d_sum=0
+    d_count=0
     while num>0:
         digit=num%10
         d_sum+=digit
+        d_count+=1
         num//=10
 
     print(f"The sum of digits is {d_sum}")
+    print(f"The count of digits is {d_count}")
     cont = input("Do you want to continue ? (yes/no) ")
     if cont.lower() !="yes":
         break
