3eefd76c6a8cae34176389c5c788f69a000669cd
Swarnava Neogi <swarnavaneogi@gmail.com>
2026-08-21 10:05:01 +0530

    Simple program showing the length of the string and doing the character access

---
+++ Strings/01_Stringlength_characteraccess.py
@@ -0,0 +1,7 @@
+name=input("Enter your name: ")
+l=len(name)
+print("Length of your name is: ",l)
+print()
+print("Accessing each character in the name:")
+for i in range (l):
+    print(f"Character at index {i} is : {name[i]}")
