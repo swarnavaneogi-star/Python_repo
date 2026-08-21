387ad707925ff22b73a4ae3ad70eb7c7dcfba173
Swarnava Neogi <swarnavaneogi@gmail.com>
2026-08-21 10:12:00 +0530

    Printing the characters of the strings

---
+++ Strings/02_character.py
@@ -0,0 +1,7 @@
+str=input("Enter a string as u wish :")
+l=len(str)
+first_character=str[0]
+last_character=str[l-1]
+print(f"Length of the string is :{l}")
+print(f"First character: {first_character}")
+print(f"Last character: {last_character}")
