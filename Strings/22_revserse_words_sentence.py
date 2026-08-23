ac88e9379c407e4136b90091cda78c4ed129258b
Swarnava Neogi <swarnavaneogi@gmail.com>
2026-08-23 12:04:45 +0530

    A program to reverse every word in a sentence

---
+++ Strings/22_revserse_words_sentence.py
@@ -0,0 +1,12 @@
+while True:
+    string=input("Enter a string :")
+    words=string.split()
+    new_sentence=""
+    for word in words :
+        reversed_word = word[::-1]
+        new_sentence += reversed_word + " "
+    print(f"The new string is : {new_sentence}")\
+
+    cont=input("Do you want to continue ? (yes/no):")
+    if cont.lower()!='yes':
+       break
