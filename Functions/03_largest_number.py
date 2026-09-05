23aec03b93dbaa17c114ee550b2397f02b3915ca
Swarnava Neogi <swarnavaneogi@gmail.com>
2026-09-05 14:42:31 +0530

    Finding the greatest number from 3 using functions

---
+++ Functions/03_largest_number.py
@@ -0,0 +1,12 @@
+def largest():
+    if (a>b) and (a>c):
+        print("The largest number is:", a)
+    elif (b>a) and (b>c):
+        print("The largest number is:", b)
+    else:
+        print("The largest number is:", c)
+
+a= float(input("Enter first number: "))
+b= float(input("Enter second number :"))
+c=float(input("Enter third number:"))
+largest()
