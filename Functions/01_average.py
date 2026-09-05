97da5b3917ad16b7a678d93f3736bd0c1bd48b2a
Swarnava Neogi <swarnavaneogi@gmail.com>
2026-09-05 12:12:57 +0530

    Calculate average with function

---
+++ Functions/01_average.py
@@ -0,0 +1,11 @@
+def avg():
+    a= float(input("Enter first number: "))
+    b= float(input("Enter second number :"))
+    c=float(input("Enter third number:"))
+
+    average = (a + b + c) / 3
+    print("The average of three numbers is: ", average)
+
+
+avg()
+print("Thank you for using the average calculator. Goodbye!")
